71f4d472fbbdbcc1e51562840bb38146b51f1020
selectofiesta <selectoelcasar@gmail.com>
2025-06-12 17:06:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let lastScrollTop = 0;
 let scrollTimeout;
 const navbar = document.querySelector(".navbar");
 
+// Detectar el scroll y ocultar/mostrar la barra de navegación
 window.addEventListener("scroll", () => {
   const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 
@@ -21,4 +22,17 @@ window.addEventListener("scroll", () => {
   scrollTimeout = setTimeout(() => {
     navbar.classList.remove("hide");
   }, 1000);
+});
+
+// Variables para manejar el expandir/contraer de la imagen
+let expandContainer = document.getElementById("expandContainer");
+let expandBtn = document.querySelector(".expand-btn");
+
+// Función para expandir o contraer la imagen de fondo y el rectángulo negro
+expandBtn.addEventListener("click", () => {
+  // Alternamos la clase "expanded" que activa la expansión de la imagen y el ocultamiento del overlay
+  expandContainer.classList.toggle("expanded");
+  
+  // También alternamos la clase "hidden" para controlar la visibilidad del rectángulo negro
+  expandContainer.classList.toggle("hidden");
 });
